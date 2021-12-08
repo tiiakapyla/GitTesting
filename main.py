0bcc1b5c6699f2b04e12fb0d2a60e348aef92f64
Tiia Käpylä <tiia.kapyla@student.samk.fi>
2021-12-08 13:14:55 +0200

    merged branches

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 # --- so the code works in terminal too ---
 #!/usr/bin/python3
 
-# --- function 'add', returns sum of two numbers ---
+# --- function 'add', returns sum of the two numbers ---
 def add(number1, number2):
     return number1 + number2
+# --- function 'deduct', returns remainder of the two numbers ---
+def deduct(number1, number2):
+    return number1 - number2
 
+# --- function 'deduction', returns remainder of the two numbers ---
 def deduction(number1, number2):
     return number1 - number2
 
@@ -13,4 +17,4 @@ number1, number2 = int(input()), int(input())
 
 # --- prints return, so it shows in terminal too
 print(add(number1, number2))
-print(deduction(number1, number2))
+print(deduct(number1, number2))
