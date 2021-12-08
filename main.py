f30ca7981ddbc5b768e9ec92dc787c6d0c7448d2
tiiakapyla <93035509+tiiakapyla@users.noreply.github.com>
2021-12-08 13:01:44 +0200

    added function 'deduct', comments
    function 'deduct' = lines 8-9, 16
    comments = added 'the' in line 5, line 7

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 # --- so the code works in terminal too ---
 #!/usr/bin/python3
 
-# --- function 'add', returns sum of two numbers ---
+# --- function 'add', returns sum of the two numbers ---
 def add(number1, number2):
     return number1 + number2
+# --- function 'deduct', returns remainder of the two numbers ---
+def deduct(number1, number2):
+    return number1 - number2
 
 # --- user input numbers 1 and 2 ---
 number1, number2 = int(input()), int(input())
 
 # --- prints return, so it shows in terminal too
 print(add(number1, number2))
+print(deduct(number1, number2))
